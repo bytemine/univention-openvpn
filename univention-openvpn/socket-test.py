4f00ae0317403f537ba7b7771de14f9ee36aa2da
Fedor Scholz <scholz@bytemine.net>
2014-10-08 16:30:07 +0200

    fork for reading and writing

--- univention-openvpn/socket-test.py
+++ univention-openvpn/socket-test.py
@@ -1,16 +1,26 @@
 import socket
 import sys
 import os
-import re
 
-command = 'state'
+def parent():
+    while True:
+        data = s.recv(8)
+        print 'recv: %s' % repr(data)
+
+def child():
+    while True:
+        cmd = raw_input('> ')
+        s.sendall(cmd)
+
+def forker():
+    newpid = os.fork()
+    if newpid == 0:
+        child()
+    else:
+        parent()
+
 socket_address = '/etc/openvpn/openvpn-socket'
-
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 s.connect(socket_address)
 
-s.sendall(command)
-
-while True:
-        data = s.recv(8)
-            print 'received: %s' % repr(data)
+forker()
