8dd4807f4565c5b1c7b8145ad9cc1714e4c42d3c
Fedor Scholz <scholz@bytemine.net>
2014-10-29 16:18:04 +0100

    add connection type

--- univention-openvpn/display_users/socket_handler.py
+++ univention-openvpn/display_users/socket_handler.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import os
 import json
+from netaddr import *
 
 def userlist():
     socket_address = '/var/run/management-udp'
@@ -19,22 +20,30 @@ def userlist():
 
     result = []
 
+    # iterate over client list
     for c in cl:
         centries = c.split('\t')
         name = centries[1]
         realaddress = centries[2]
 
+	conntype = 0
         virtaddresses = ""
 
+	# iterate over routing table to get all virtual addresses for each client
         for r in rt:
             rentries = r.split('\t')
+	    rvirtaddress = rentries[1]
             rname = rentries[2]
             rrealaddress = rentries[3]
             
             if name == rname and realaddress == rrealaddress:
-                virtaddresses += rentries[1] + "\n"
+		if IPAddress(rvirtaddress).version == 4:
+		    conntype |= 1
+		elif IPAddress(rvirtaddress).version == 6:
+		    conntype |= 2
+                virtaddresses += rvirtaddress + "\n"
 
-        result.append({'name': name, 'connected': 'True', 'realip': realaddress, 'virtips': virtaddresses, 'recv': centries[4], 'sent': centries[5], 'cons': centries[6], 'cont': centries[7]})
+        result.append({'name': name, 'conn': 1, 'type': conntype, 'realip': realaddress, 'virtips': virtaddresses, 'cons': centries[6], 'cont': centries[7], 'recv': centries[4], 'sent': centries[5]})
 
     return result
 
