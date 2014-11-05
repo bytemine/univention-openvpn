a719457e776d57b052b28a1d087450973930666a
Fedor Scholz <scholz@bytemine.net>
2014-11-05 16:54:33 +0100

    add new syntax for site-to-site

--- univention-openvpn/univention-openvpn-schema.py
+++ univention-openvpn/univention-openvpn-schema.py
@@ -10,10 +10,10 @@ class openvpnUserandAddress( complex ):
         delimiter = ':'
         all_required = 0
         description=_('Openvpn user')
-        subsyntaxes = ( ( _( 'Openvpn user' ), openvpnUser ), ( _( 'IPv4 address' ), ipv4Address ), ( _( 'IPv6 address' ), ipAddress ), )
+        subsyntaxes = [ ( _( 'Openvpn user' ), openvpnUser ), ( _( 'IPv4 address' ), ipv4Address ), ( _( 'IPv6 address' ), ipAddress ) ]
 
 class IPTuple( complex ):
         delimiter = ':'
         all_required = 0
         description=_('Openvpn ifconfig')
-        subsyntaxes = ( ( _( 'Local IP address' ), ipAddress ), ( _( 'Remote IP address' ), ipAddress ), )
+        subsyntaxes = [ ( _( 'Local IP address' ), ipAddress ), ( _( 'Remote IP address' ), ipAddress ) ]
