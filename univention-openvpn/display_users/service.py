72d3825fbc762d62bf8135104f697a9f2345bb7c
Holger Rasch <rasch@bytemine.net>
2015-02-25 13:19:41 +0100

    fix 'kill_user' service return json, catch exceptions (#3482)

--- univention-openvpn/display_users/service.py
+++ univention-openvpn/display_users/service.py
@@ -16,6 +16,31 @@ urls = (
 )
 app = MyApplication(urls, globals())
 
+
+def connected_users(name):
+    listener.setuid(0)
+    lo = ul.getBackupConnection()
+    users = lo.search('univentionOpenvpnAccount=1')
+    users = map(lambda user: "%s.openvpn" % user[1].get('uid', [None])[0], users)
+    listener.unsetuid()
+    connected_users = userlist()
+
+    # append not connected users
+    for user in users:
+        if not any(u['name'] == user for u in connected_users):
+            connected_users.append({'name': user, 'connected': 0, 'type': 0, 'realip': '', 'virtips': '', 'cons': '', 'conr': '', 'recv': 0, 'sent': 0})
+    count = str(len(connected_users))
+
+    query = web.ctx.query
+    if query:
+        # jsonp
+        queries = query.split('&')
+        callback = queries[0].split('=')[1]
+        return '%s({"draw": 1, "recordsTotal": %s, "recordsFiltered": %s, "data": %s});' % (callback, count, count, json.dumps(connected_users))
+    else:
+        return '{"data": %s}' % json.dumps(connected_users)
+
+
 class display_users:
     def GET(self, name):
 
@@ -23,35 +48,20 @@ class display_users:
         name_pieces = name.split('/')
 
         if 'connected_users' == name_pieces[0]:
-
-            listener.setuid(0)
-            lo = ul.getBackupConnection()
-            users = lo.search('univentionOpenvpnAccount=1')
-            users = map(lambda user: "%s.openvpn" % user[1].get('uid', [None])[0], users)
-            listener.unsetuid()
-            connected_users = userlist()
-
-            # append not connected users
-            for user in users:
-                if not any(u['name'] == user for u in connected_users):
-                    connected_users.append({'name': user, 'connected': 0, 'type': 0, 'realip': '', 'virtips': '', 'cons': '', 'conr': '', 'recv': 0, 'sent': 0})
-            count = str(len(connected_users))
-
-            query = web.ctx.query
-            if query:
-                # jsonp
-                queries = query.split('&')
-                callback = queries[0].split('=')[1]
-                return '%s({"draw": 1, "recordsTotal": %s, "recordsFiltered": %s, "data": %s});' % (callback, count, count, json.dumps(connected_users))
-            else:
-                return '{"data": %s}' % json.dumps(connected_users)
+            return connected_users(name)
 
         elif 'kill_user' == name_pieces[0]:
-            id = name_pieces[1]
-            kill_answer = killuser(id)
-            return "{'message': %s}" % kill_answer
+            try:
+                id = name_pieces[1]
+                kill_answer = killuser(id)
+            except:
+                pass
+            return connected_users(name)
+
         else:
-            return "{'message': 'unknown command'}"
+            return connected_users(name)
+
 
 if __name__ == "__main__":
     app.run(port=38081)
+
