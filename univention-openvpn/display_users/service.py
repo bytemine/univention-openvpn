e9bc7d378a81c5d209dd5e8920920b2333080ad4
Fedor Scholz <scholz@bytemine.net>
2015-11-27 12:55:28 +0100

    and even more

--- univention-openvpn/display_users/service.py
+++ univention-openvpn/display_users/service.py
@@ -36,6 +36,7 @@ from socket_handler import *
 from datetime import date
 from M2Crypto import RSA, BIO
 from base64 import b64decode
+import univention_openvpn_common
 
 # turn off debug mode (exceptions as html pages)
 web.config.debug = False
@@ -50,47 +51,6 @@ urls = (
 )
 app = MyApplication(urls, globals())
 
-
-pubbio = BIO.MemoryBuffer('''
------BEGIN PUBLIC KEY-----
-MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN0VVx22Oou8UTDsrug/UnZLiX2UcXeE
-GvQ6kWcXBhqvSUl0cVavYL5Su45RXz7CeoImotwUzrVB8JnsIcrPYw8CAwEAAQ==
------END PUBLIC KEY-----
-''')
-pub = RSA.load_pub_key_bio(pubbio)
-pbs = pub.__len__() / 8
-
-def license(key):
-  try:
-    enc = b64decode(key)
-    raw = ''
-    while len(enc) > pbs:
-      d, key = (enc[:pbs], enc[pbs:])
-      raw = raw + pub.public_decrypt(d, 1)
-    if len(enc) != pbs:
-      return None               # invalid license
-    raw = raw + pub.public_decrypt(enc, 1)
-    items = raw.rstrip().split('\n')
-    if not items:
-      return None               # invalid license
-    vdate = int(items.pop(0))
-    if date.today().toordinal() > vdate:
-      return None               # expired
-    l = {'valid': True, 'vdate': vdate} # at least one feature returned
-    while items:
-      kv = items.pop(0).split('=', 1)
-      kv.append(True)
-      l[kv[0]] = kv[1]
-    return l                    # valid license
-  except:
-    return None                 # invalid license
-
-def maxvpnusers(key):
-  mnlu = 5
-  try:
-    return max(int(license(key)['u']), mnlu)
-  except:
-    return mnlu
 
 def connected_users():
     listener.setuid(0)
@@ -138,9 +98,9 @@ def license_stats():
 
     c_connected_users = len(connected_users)
     c_users = len(users)
-    c_licenced = maxvpnusers(key)
+    c_licenced = univention_openvpn_common.maxvpnusers(key)
     try:
-        l = license(key)
+        l = univention_openvpn_common.license(key)
         valid = str(date.fromordinal(l['vdate']))
     except:
         valid = "No valid license"
