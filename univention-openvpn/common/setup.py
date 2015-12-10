dc251a5f7eadfc437fb5585af3a5f89f77f6fc8c
Fedor Scholz <scholz@bytemine.net>
2015-12-10 14:16:12 +0100

    use correct author information

--- univention-openvpn/common/setup.py
+++ univention-openvpn/common/setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(name='univention_openvpn_common',
       version='0.1',
       description='library for univention-openvpn',
+      author='bytemine GmbH',
+      author_email='info@bytemine.net',
+      url='https://www.bytemine.net/de/leistungen/openvpn4ucs.html',
       packages=['univention_openvpn_common'],
       zip_safe=False)
