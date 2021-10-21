d3212d7c7e961b782c85e2abf6f26ef3baf858ed
Holger Rasch <rasch@bytemine.net>
2021-10-21 13:01:53 +0200

    univnention_openvpn_common is gone

--- univention-openvpn/refresh-bundles.py
+++ univention-openvpn/refresh-bundles.py
@@ -32,34 +32,83 @@
 
 from os import system
 import univention.uldap as ul
-from univention_openvpn_common import maxvpnusers
+
+from datetime import date
+from M2Crypto import RSA, BIO
+from base64 import b64decode
+
+MAX_UNLIC_USERS = 5
 
 
-lo = ul.getMachineConnection()
+def maxvpnusers(key):
+    try:
+        return max(int(license(key)['u']), MAX_UNLIC_USERS)
+    except:
+        return MAX_UNLIC_USERS
 
-maxu = 5
-lobs = lo.search('(univentionOpenvpnLicense=*)')
-for lob in lobs:
-    key = lob[1].get('univentionOpenvpnLicense', [b''])[0]
-    mu = maxvpnusers(0, key)
-    if mu > maxu: maxu = mu
 
-vpnusers = lo.search('(univentionOpenvpnAccount=1)')
-if len(vpnusers) > maxu:
-    exit()
+def main():
+    lo = ul.getMachineConnection()
 
-vpnservers = lo.search('(&(objectClass=univentionOpenvpn)(univentionOpenvpnActive=1))')
+    lobs = lo.search('(univentionOpenvpnLicense=*)')
+    lul = []
+    for lob in lobs:
+        key = lob[1].get('univentionOpenvpnLicense', [b''])[0]
+        lul.append(maxvpnusers(key))
 
-for (tmp, server) in vpnservers:
-    name = server.get('cn', [b''])[0]
-    port = server.get('univentionOpenvpnPort', [b''])[0]
-    addr = server.get('univentionOpenvpnAddress', [b''])[0]
-    if not name or not port or not addr:
-        continue
-    for user in vpnusers:
-        uid = user[1].get('uid', [b''])[0]
-        proto = b'udp6' if addr and addr.count(b':') else b'udp'
-        if uid:
-            system(b'/usr/lib/openvpn-int/create-bundle %s %s %s %s %s' % (uid, name, addr, port, proto))
+    vpnusers = lo.search('(univentionOpenvpnAccount=1)')
+    if len(vpnusers) > max(lul):
+        exit()
+
+    vpnservers = lo.search('(&(objectClass=univentionOpenvpn)(univentionOpenvpnActive=1))')
+    for (tmp, server) in vpnservers:
+        name = server.get('cn', [b''])[0]
+        port = server.get('univentionOpenvpnPort', [b''])[0]
+        addr = server.get('univentionOpenvpnAddress', [b''])[0]
+        if not name or not port or not addr:
+            continue
+        for user in vpnusers:
+            uid = user[1].get('uid', [b''])[0]
+            proto = b'udp6' if addr and addr.count(b':') else b'udp'
+            if uid:
+                system(b'/usr/lib/openvpn-int/create-bundle %s %s %s %s %s' % (uid, name, addr, port, proto))
+
+
+def license(key):
+    enc = b64decode(key)
+    raw = ''
+    while len(enc) > pbs:
+        d, key = (enc[:pbs], enc[pbs:])
+        raw = raw + pub.public_decrypt(d, 1).decode('utf8')
+    if len(enc) != pbs:
+        return None
+    raw = raw + pub.public_decrypt(enc, 1).decode('utf8')
+    #
+    items = raw.rstrip().split('\n')
+    if not items:
+        return None
+    vdate = int(items.pop(0))
+    if date.today().toordinal() > vdate:
+        return None
+    l = {'valid': True, 'vdate': vdate} # at least one feature returned
+    while items:
+        kv = items.pop(0).split('=', 1)
+        kv.append(True)
+        l[kv[0]] = kv[1]
+    return l
+
+
+pubbio = BIO.MemoryBuffer(b'''
+-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN0VVx22Oou8UTDsrug/UnZLiX2UcXeE
+GvQ6kWcXBhqvSUl0cVavYL5Su45RXz7CeoImotwUzrVB8JnsIcrPYw8CAwEAAQ==
+-----END PUBLIC KEY-----
+''')
+
+if __name__ == '__main__':
+    pub = RSA.load_pub_key_bio(pubbio)
+    pbs = pub.__len__() / 8
+    main()
+
 
 ### end ###
