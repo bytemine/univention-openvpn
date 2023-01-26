fa6918eb78d431f7a559ae6390e4f89fd32e359c
Holger Rasch <rasch@bytemine.net>
2023-01-26 16:11:50 +0100

    more control over parameters (block size)

--- univention-openvpn/o4u_qrcgen.py
+++ univention-openvpn/o4u_qrcgen.py
@@ -11,8 +11,11 @@ f = open('/etc/openvpn/mfa/secrets')
 for l in f:
     try:
         u, s = l.rstrip().split(':')[:2]
-        x = qrcode.make('otpauth://totp/bytemine.net:{}?secret={}&issuer=bytemine.net&digits=6'.format(u, s))
+        q = qrcode.QRCode(box_size=4)
+        q.add_data('otpauth://totp/bytemine.net:{}?secret={}&issuer=bytemine.net&digits=6'.format(u, s))
+        q.make (fit=True)
         pngpath = '/var/www/readytogo/{}/qrcode.png'.format(u)
+        x = q.make_image)
         x.save(pngpath)
         os.chmod(pngpath, 0640)
         uid = pwd.getpwnam(u).pw_uid
