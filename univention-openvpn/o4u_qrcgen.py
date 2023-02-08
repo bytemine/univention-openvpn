6d15b7a8580afbabf332068b5503e0266d0a33d4
Holger Rasch <rasch@bytemine.net>
2023-02-08 12:44:59 +0100

    do output, again - slightly larger png

--- univention-openvpn/o4u_qrcgen.py
+++ univention-openvpn/o4u_qrcgen.py
@@ -11,7 +11,7 @@ f = open('/etc/openvpn/mfa/secrets')
 for l in f:
     try:
         u, s = l.rstrip().split(':')[:2]
-        q = qrcode.QRCode(box_size=4)
+        q = qrcode.QRCode(box_size=5)
         q.add_data('otpauth://totp/bytemine.net:{}?secret={}&issuer=bytemine.net&digits=6'.format(u, s))
         q.make (fit=True)
         pngpath = '/var/www/readytogo/{}/qrcode.png'.format(u)
@@ -21,6 +21,7 @@ for l in f:
         uid = pwd.getpwnam(u).pw_uid
         gid = grp.getgrnam('www-data').gr_gid
         os.chown(pngpath, uid, gid)
+        print('generated qrcode for {}'.format(repr(u)))
     except:
         traceback.print_exc()
         print('ignoring line \'{}\''.format(l))
