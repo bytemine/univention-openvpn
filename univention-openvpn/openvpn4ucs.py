d9b4e7d860604e6c1803ac1de8463642bae0d23a
Holger Rasch <rasch@bytemine.net>
2023-03-03 16:17:02 +0100

    drop .py suffix for o4u_auth and include it in the package

--- univention-openvpn/openvpn4ucs.py
+++ univention-openvpn/openvpn4ucs.py
@@ -647,7 +647,7 @@ management /var/run/management-udp unix
 dev tun
 topology subnet
 script-security 2
-auth-user-pass-verify /usr/lib/openvpn-int/o4u_auth.py via-file
+auth-user-pass-verify /usr/lib/openvpn-int/o4u_auth via-file
 auth-gen-token 50000
 
 ### Values which can be changed through UDM
